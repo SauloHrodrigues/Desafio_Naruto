ea9fb18aec96079680845cac306f8a9f8d71117d
saulo.rodrigues <saulohrodrigues@gmail.com>
2024-12-27 10:17:22 -0300

    Subindo a entidade Personagem

---
+++ src/main/java/com/projetonaruto/model/Personagem.java
@@ -0,0 +1,45 @@
+package com.projetonaruto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Personagem {
+    private String nome;
+    private int idade;
+    private String aldeia;
+    private List<String> jutsus = new ArrayList<>();
+    private int chakra;
+
+    public Personagem(String nome, int idade, String aldeia, String jutsus, int chakra) {
+        this.nome = nome;
+        this.idade = idade;
+        this.aldeia = aldeia;
+        this.jutsus.add(jutsus);
+        this.chakra = chakra;
+    }
+
+    public boolean adicionarJutsu(String jutsu){
+        try {
+            jutsus.add(jutsu);
+        }catch (RuntimeException e){
+            return false;
+        }
+        return true;
+    }
+
+    public boolean aumentarChakra(int chakra){
+        this.chakra += chakra;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Personagem{" +
+                "nome: '" + nome + '\'' +
+                ", idade: " + idade +
+                ", aldeia: '" + aldeia + '\'' +
+                ", jutsus: " + jutsus +
+                ", chakra: " + chakra +
+                '}';
+    }
+}
