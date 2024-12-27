8c044f8d198c03691f3c15a55e0ba46c61833bf3
saulo.rodrigues <saulohrodrigues@gmail.com>
2024-12-27 09:20:32 -0300

    Subindo a estrutura inicial do projeto

---
+++ src/test/java/com/projetonaruto/NarutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetonaruto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NarutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
