7b7321fa458a2340d561d20b554b10c421545acd
saulo.rodrigues <saulohrodrigues@gmail.com>
2025-01-29 17:12:36 -0300

    Refatorada a Cslsse Pds einjas arpa adequa-la as documentação da segunda fase do projeto

--- src/main/java/com/projetonaruto/model/NinjaGenjutsu.java
+++ src/main/java/com/projetonaruto/model/NinjaGenjutsu.java
@@ -2,6 +2,7 @@ package com.projetonaruto.model;
 
 import com.projetonaruto.enuns.CategoriaJutsuEnum;
 import com.projetonaruto.interfaces.Ninja;
+import java.util.Map;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,12 +10,8 @@ import lombok.Setter;
 @Setter
 public class NinjaGenjutsu extends Personagem implements Ninja {
 
-    public NinjaGenjutsu() {
-        super();
-    }
-
-    public NinjaGenjutsu(String nome, int idade, String aldeia, String jutsus, int chakra) {
-        super(nome, idade, aldeia, jutsus, chakra);
+    public NinjaGenjutsu(String nomeNinja, int idade, String aldeia, boolean ninja) {
+        super(nomeNinja, idade, aldeia, ninja);
     }
 
     @Override
@@ -26,4 +23,34 @@ public class NinjaGenjutsu extends Personagem implements Ninja {
     public String desviar() {
         return "o personagem está desviando de um ataque usando sua habilidade em NinjaGenjutsu";
     }
+
+    public Map<String, Jutsu> getMapJutsus() {
+        return jutsus;
+    }
+
+    public void setMapJutsus(Map<String, Jutsu> jutsus) {
+        this.jutsus = jutsus;
+    }
+
+    public int getPChakra() {
+        return chakra;
+    }
+
+
+    public Map<String, Jutsu> getJutsus() {
+        return jutsus;
+    }
+
+    public void setJutsus(Map<String, Jutsu> jutsus) {
+        this.jutsus = jutsus;
+    }
+
+    public int getChakra() {
+        return chakra;
+    }
+
+    public void setChakra(int chakra) {
+        this.chakra = chakra;
+    }
+
 }
